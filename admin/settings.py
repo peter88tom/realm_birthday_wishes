2944ed312525214e7880798899ecab9c74d4e81e
peter88tom <peter88tom@gmail.com>
2022-04-05 01:13:23 +0200

    Update static files path

--- admin/settings.py
+++ admin/settings.py
@@ -130,8 +130,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/opt/homebrew/var/www/python_projects/realm_birthday_wishes/static/'
+    os.path.join(BASE_DIR, "static/")
 ]
 
 # Default primary key field type
